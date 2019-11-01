ad544f65af3788ee35af82efaf5e0c2a71086dd5
selam <selam0315@gmail.com>
2019-11-01 13:47:26 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generics_Gray_Garage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
